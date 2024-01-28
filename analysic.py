2250791e0ea9fb939372bf83b1d3820f7da43a21
bnpysse <bnpysse@aliyun.com>
2024-01-28 20:10:19 +0800

    Test one

--- analysic.py
+++ analysic.py
@@ -1,4 +1,8 @@
+# 给定一个 URL, 形成 recycle,
+# from 1970 to 2021, 10年一个周期
+#
 import time
+
 import requests
 
 method = 'listResults'
@@ -10,18 +14,19 @@ agreeToTsAndCs = 'Yes'
 exportToSpreadsheet = 'Yes'
 
 url = 'https://www2.bgs.ac.uk/mineralsuk/statistics/wms.cfc?method={method}&dataType={dataType}&dateFrom={dateFrom}&dateTo={dateTo}&commodity={commodity}&country=&agreeToTsAndCs=agreed&exportToSpreadsheet=Yes'
-commodity_list = [2,4,5,6,199]
+commodity_list = [2, 4, 5, 6, 199]
 
 for comm in commodity_list:
-    for year in range(1970,2024,10):
-        dateFrom, dateTo = year, year+9
-        dateTo = 2021 if dateTo>=2021 else dateTo
+    for year in range(1970, 2024, 10):
+        dateFrom, dateTo = year, year + 9
+        dateTo = 2021 if dateTo >= 2021 else dateTo
         # print(dateFrom,dateTo)
-        print(url.format(method=method,dateFrom=dateFrom,dateTo=dateTo,commodity=comm,dataType=dataType))
-        down_res = requests.get(url.format(method=method,dateFrom=dateFrom,dateTo=dateTo,commodity=comm,dataType=dataType))
+        print(url.format(method=method, dateFrom=dateFrom, dateTo=dateTo, commodity=comm, dataType=dataType))
+        down_res = requests.get(
+            url.format(method=method, dateFrom=dateFrom, dateTo=dateTo, commodity=comm, dataType=dataType))
         if 'No results' not in str(down_res.content):
-            with open(f'{comm}_{dateFrom}.xlsx','wb') as f:
+            with open(f'{comm}_{dateFrom}.xlsx', 'wb') as f:
                 f.write(down_res.content)
             time.sleep(10)
         else:
-            print('No results,so can\'t create the file!')
+            print('No results,so can\'t create the filesymotion-lineforward!')
